4d3daac69e365a71ba18a1f5fe3973fb1d484933
ntak <ntak@starship.net>
2021-02-08 21:06:36 +0900

    type-missing...

--- Core/Base/AppView.php
+++ Core/Base/AppView.php
@@ -243,7 +243,7 @@ public function ViewTemplate($name,$vars = []) {
                     $var = $m[1];
                     $arg = (count($m)===3) ? $m[2]:NULL;
                     if(method_exists($this->Helper,$var)) {
-                        $val = $this->Helper-$var($arg);
+                        $val = $this->Helper->$var($arg);
                     } else $val = "NOT-FOUND({$var})";
                     break;
             default:
@@ -741,6 +741,7 @@ debug_log(-899,['SEC'=>$sec,'SUB'=>$subsec,'ATTR'=>$attrs,'TXT'=>$text]);
     //  ] ]
     private function cmd_select($tag,$attrs,$sec,$vars) {
         if(!is_array($sec)) return;     // not allow scalar value
+        list($attrs,$text,$sec) = $this->subsec_separate($sec,$attrs,$vars);
         $attr = $this->gen_Attrs($attrs,$vars);
         echo "<{$tag}{$attr}>\n";
         list($opt_key, $opt_val) = array_first_item($sec);
