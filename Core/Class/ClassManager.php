c028b645b89626975d4f10033f23b84bef6d4997
ntak <ntak@starship.net>
2021-03-05 17:11:31 +0900

    class management parent on array.

--- Core/Class/ClassManager.php
+++ Core/Class/ClassManager.php
@@ -6,17 +6,18 @@ class ClassManager {
     private static $ObjectList = [];  // [ 'ClassName' => [ state, ini_cnt, ref_count, owner, Object] ]
 //==============================================================================
 public static function Create($module_name,$class_name,$owner) {
+    $oname = ($owner === NULL) ? '(Main)' :$owner->ClassName;
     if(array_key_exists($module_name,static::$ObjectList)) {
-        list($state,$ini,$cnt,$om,$obj) = static::$ObjectList[$module_name];
+        list($state,$ini,$cnt,$owlist,$obj) = static::$ObjectList[$module_name];
         if($state !== 0) {
             if(++$ini > 2 ) die("Conflict '{$module_name}' Create during initializing @ {$this->ClassName}.\n");
         } else $ini = 0;
-        static::$ObjectList[$module_name] = [ $state,$ini,++$cnt,$om,$obj];
+        if(!in_array($oname,$owlist)) $owlist[] = $oname;
+        static::$ObjectList[$module_name] = [ $state,$ini,++$cnt,$owlist,$obj];
         return $obj;
     }
     $obj = new $class_name($owner);
-    $om = ($owner === NULL) ? '(Main)' :$owner->ClassName;
-    static::$ObjectList[$module_name] = [ 1, 1, 1,$om, $obj];
+    static::$ObjectList[$module_name] = [ 1, 1, 1,[$oname], $obj];
     if(method_exists($obj,'class_startup')) {
         $obj->class_startup();
     }
@@ -27,13 +28,14 @@ public static function Create($module_name,$class_name,$owner) {
 //==============================================================================
 // Dump ObjectList
 public static function DumpObject() {
-    $dmp = "[ STATE, INI, REF, OWNER ]\n";
+    $dmp = "[ STATE, INI, REF ] @ OWNER\n";
     $padding = str_repeat(" ", 16);
 //    ksort(static::$ObjectList);
     foreach(static::$ObjectList as $key => $val) {
-        list($state,$ini,$cnt,$om) = $val;
+        list($state,$ini,$cnt,$ow_list) = $val;
+        $om = implode(', ',$ow_list);
         $kk = substr("{$key}{$padding}",0,16);
-        $dmp .= "  {$kk} = [ {$state}, {$ini}, {$cnt}, {$om} ]\n";
+        $dmp .= "  {$kk} = [ {$state}, {$ini}, {$cnt} ]@( {$om} )\n";
     }
     return $dmp;
 }
