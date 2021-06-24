b32eccb7a3e19f794f3777fe0c1c039887bd3b93
ntak <ntak@starship.net>
2021-06-24 23:41:47 +0900

    datebox event entry.

--- Core/Template/res/js/window.js
+++ Core/Template/res/js/window.js
@@ -12,6 +12,28 @@ selector.each(function () {
 		self.css("top", top + "px");
 	});
 });
+// カレンダー設定
+$(".calendar").each(function () {
+	var self = $(this); // jQueryオブジェクトを変数に代入しておく
+	var date_form = {
+		dateFormat: 'yy-mm-dd',
+		monthNames: [${#.core.monthNames}],
+		dayNamesMin: [${#.core.dayNames}],
+		yearSuffix: "${#.core.YearSuffix}",
+		buttonImage: "/res/images/calender_icon.png",   // カレンダーアイコン画像
+		buttonImageOnly: true,           // 画像として表示
+		showOn: "button",                   // カレンダー呼び出し元の定義
+		buttonText: "${#.core.ToolTip}", // ツールチップ表示文言
+		showMonthAfterYear: true,
+	};
+	if (self.hasClass('no_icon')) {
+		delete date_form.buttonImage;
+		delete date_form.buttonImageOnly;
+		delete date_form.showOn;
+		delete date_form.buttonText;
+	};
+	self.datepicker(date_form);
+});
 // ウィンドウ高さ調整
 $('body').fitWindow();
 // マークダウン外部リンク
@@ -23,4 +45,5 @@ $(document).on('change','.combobox>select',function () {
 	var txt = $(this).find('option:selected').html();
 	var inbox = $(this).parent().find('input');
 	inbox.val(txt);
+	inbox.addClass('modified');
 });
