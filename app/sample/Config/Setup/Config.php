69b94b60bef2a64bc3023cc8efdae3708f75636d
ntak <ntak@starship.net>
2021-07-25 20:26:21 +0900

    separatre database define.

--- app/sample/Config/Setup/Config.php
+++ app/sample/Config/Setup/Config.php
@@ -1,42 +1,6 @@
 <?php
-//--------------------------------------------
-// SQLite3データベース
-define('SQLITE_DB',__DIR__ . '/../sample.db');
-//--------------------------------------------
-// PostgreSQLのサーバー定義
-define('PG_HOST','localhost');
-define('PG_PORT',  '5432');
-define('PG_DB',  'pg_blog');
-//--------------------------------------------
-// MySQLのサーバー定義
-define('MYSQL_DB',  'my_blog');
-
-define('HANDLER',  'SQLite');
-
-const DatabaseParameter  = [
-	'Postgre' =>  array(
-		'host' => PG_HOST,
-		'port' => PG_PORT,
-		'database' => PG_DB,
-		'login' => 'postgres',
-		'password' => 'postgres',
-		'encoding' => 'utf8'
-	),
-	'SQLite' => array(
-		'host' => 'localhost',
-		'database' => SQLITE_DB,
-		'login' => '',
-		'password' => '',
-		'encoding' => 'utf8'
-	),
-	'MySQL' => array(
-		'host' => 'localhost',
-		'database' => MYSQL_DB,
-		'login' => 'admin',
-		'password' => 'admin',
-		'encoding' => 'utf8'
-	),
-];
+// データベース定義をインポート
+require_once(__DIR__ . '/../database.php');
 
 //==============================================================================
 // クラス名とファイルの対応表
