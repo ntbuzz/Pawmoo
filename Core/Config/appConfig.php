03e122ed2d4a6bc7df4cfd59542edc241657eb34
ntak <ntak@starship.net>
2021-08-22 13:49:36 +0900

    revision document update.

--- Core/Config/appConfig.php
+++ Core/Config/appConfig.php
@@ -3,7 +3,7 @@
  * Object Oriented PHP MVC Framework
  *  appConfig: Framework Configuration
  */
-define('CURRENT_VERSION','0.82 2021-08-13');
+define('CURRENT_VERSION','0.83 2021-08-22');
 define('COPYTIGHT','Copyright (c) 2017 - 2021 by nTak');
 define('PLATFORM_NAME','pawmoo');
 define('SESSION_PREFIX','_minimvc_pawmoo_maps');
