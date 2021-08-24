441a5b91681411b9b7dd680e023bf32741d0941a
ntak <ntak@starship.net>
2021-08-24 18:19:35 +0900

    checkbox compare in-string %

--- Core/Base/AppView.php
+++ Core/Base/AppView.php
@@ -883,6 +883,9 @@ public function ViewTemplate($name,$vars = []) {
 							$cmp2 = intval($m[1]);
 							$cmp1 = intval($cmp1);
 	                        $checked = $check_func(($cmp1 & $cmp2)!==0);
+						} else if(mb_substr($cmp2,0,1) === '%') {	// include value check
+							$cmp2 = mb_substr($cmp2,1);
+							$checked = $check_func(mb_strpos($cmp1,$cmp2) !== FALSE);
 						} else $checked = $check_func($cmp1 === $cmp2);
                     } else $checked = $check_func(!empty($val));
                 } else if($key[0]==='@') {
