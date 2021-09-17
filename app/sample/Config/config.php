ee0bc4d1e3d4f0d70f214d37c07ce54cbe764728
ntak <ntak@starship.net>
2021-09-17 11:05:19 +0900

    help icon changed.

--- app/sample/Config/config.php
+++ app/sample/Config/config.php
@@ -5,3 +5,11 @@ define('FORCE_REDIRECT', FALSE);
 
 define('DEFAULT_CONTROLLER', 'index');
 define('DEFAULT_LANG', 'ja');				// 言語ファイル
+
+define('LOCALE_REGION', [
+	'ja'	=> 'ja.jp',			// Japan
+	'en'	=> 'en.us',			// United State
+	'fr'	=> 'en.fr',			// France
+	'de'	=> 'en.de',			// German
+	'zh'	=> 'en.cn',			// Chinese
+]);
