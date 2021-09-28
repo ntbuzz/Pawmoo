bb3390aca7704486a74d7db729a56aeac25742b5
ntak <ntak@starship.net>
2021-09-28 21:10:38 +0900

    Paging judge spec change.

--- Core/Base/AppController.php
+++ Core/Base/AppController.php
@@ -182,28 +182,29 @@ public function ActionDispatch($action) {
 }
 //==============================================================================
 // Auto Paging.
-// Pager Break: num == 0 && sz == 0
-//				method/filter change (PagingPath())
-//				POST/GET content exists
+// Pager Break: URI != prev-URI
+//				Saved-COND === NULL
+//				COND not NULL AND Saved-COND != COND
+//				COND Record-Count < page-size
+// Page-Size param POST, it willbe setup App::$Params[1]
+//
 public function AutoPaging($cond, $max_count = 100) {
-	list($num,$size) = array_intval(App::$Params);
-	$cond = re_build_array($cond);
+	list($num,$size) = array_intval(App::$Params);		// from URL parameter .../page#/size#
+	// check SAVED Paging-Param
 	$Page = MySession::getPagingIDs('Setup');
 	list($sCond,$sSize,$sURI,$sQuery) = array_filter_values($Page,['Cond','Size','URI','QUERY']);
-	$uri = App::Get_PagingPath();
-	if( ($num|$size) === 0 || !App::$emptyRequest  || $uri !== $sURI) {
-		// NEW Paging START
-		if($num === 0) $size = 0;		//  Page# none will be Pageing-CANCEL
-		else if($size === 0) $size = $max_count;
+	if($num === 0) $num = 1;
+	if($size === 0) {
+		$size = intval($sSize);
+		if($size === 0) $size = $max_count;
+	}
+	if($uri !== $sURI || empty($sCond) || (!empty($cond) && $sCond !== $cond))  {
 		$Page['Cond'] = $cond;
 		$Page['URI'] = $uri;
 		$Page['QUERY'] = App::$Query;
-	} else {	// Pager Continued
-		if($size === 0) {
-			$size = intval($sSize);		// saved Page-Size
-			if($size === 0) $size = $max_count;
-		}
-		$cond = $sCond;			// previous saved condition
+	} else {
+		$cond = $sCond;				// repeat by saved condition
+		if($size !== intval($sSize)) $num = 1;	// different size must be jump to Page-1
 		$this->SetHelperProps(['Query' => $sQuery]);	// Query is set to HELPER Props
 	}
 	$cnt = $this->Model->getCount($cond);
@@ -233,8 +234,9 @@ public function ListAction() {
 //==============================================================================
 // Default Page Action
 public function PageAction() {
-	$this->AutoPaging([],50);
-	$this->ListAction();
+	$cond = $this->AutoPaging([], 50);
+	$this->Model->RecordFinder($cond,NULL);
+	$this->View->PutLayout();
 }
 //==============================================================================
 // Default Find Action
