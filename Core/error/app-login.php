52035c21d8a1dbaf872f4048d08440ce6a102085
ntak <ntak@starship.net>
2021-10-01 14:03:51 +0900

    reset pass button add.

--- Core/error/app-login.php
+++ Core/error/app-login.php
@@ -13,16 +13,6 @@
         <?= $msg_body ?>
         <hr>
     <div style="display:flex;">
-<<<<<<< HEAD
-
-        <form method="POST">
-        <table>
-            <tr><th><?= $user_title; ?>:</th><td><input type="text" name="userid" value="<?= $login_user; ?>"></td></tr>
-            <tr><th><?= $pass_title; ?>：</th><td><input type="password" name="password" value=""></td>
-	<td>パスワード発行: <a href="./pass-reset.html">こちら</a></td>
-</tr>
-            <tr><td colspan="2" align="center"><input type="submit" value="<?= $send_button; ?>"></td></tr>
-=======
         <form method="POST">
         <table>
             <tr><th><?= $user_title; ?>:</th><td><input type="text" name="userid" value="<?= $login_user; ?>"></td></tr>
@@ -33,7 +23,6 @@
 			</td>
 			<td><input type="submit" name="reset" value="<?= $reset_button; ?>" formaction="./pass-reset.html"></td>
 			</tr>
->>>>>>> dev/master
         </table>
         </form>
     </div>
