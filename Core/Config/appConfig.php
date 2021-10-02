b73958101a620f2febd94eb932bd0e3443abd7c8
ntak <ntak@starship.net>
2021-10-02 18:22:23 +0900

    Master (#124)
    - revision cofig/history update

--- Core/Config/appConfig.php
+++ Core/Config/appConfig.php
@@ -3,7 +3,7 @@
  * Object Oriented PHP MVC Framework
  *  appConfig: Framework Configuration
  */
-define('CURRENT_VERSION','0.92 2021-09-28');
+define('CURRENT_VERSION','0.93 2021-10-02');
 define('COPYTIGHT','Copyright (c) 2017 - 2021 by nTak');
 define('PLATFORM_NAME','pawmoo');
 define('SESSION_PREFIX','_minimvc_pawmoo_maps');
