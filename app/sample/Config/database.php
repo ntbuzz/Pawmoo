1f13d1ad7b44cd5b01b6b1def2831da95ce4d5bd
ntak <ntak@starship.net>
2022-01-26 22:39:11 +0900

    Global-Configの共通定義に対応

--- app/sample/Config/database.php
+++ app/sample/Config/database.php
@@ -9,42 +9,39 @@ define('SQLITE_DB',__DIR__ . '/sample.db');
 //--------------------------------------------
 
 const GlobalConfig = [
+// 共通設定
+'Postgre' =>  [
+	'host' => 'localhost',
+	'database' => 'pg_sample',
+	'login' => 'postgres',
+	'password' => 'postgres',
+	'encoding' => 'utf8',
+],
+'SQLite' => [
+	'host' => 'localhost',
+	'database' => SQLITE_DB,
+	'login' => '',
+	'password' => '',
+	'encoding' => 'utf8',
+],
+'MySQL' => [
+	'host' => 'localhost',
+	'database' => 'my_sample',
+	'login' => 'admin',
+	'password' => 'admin',
+	'encoding' => 'utf8',
+],
 'production' => [
 	'USE_DEBUGGER'	=> false,
 	'SESSION_LIMIT' => 'tomorrow 03:00:00',
 	'Postgre' =>  [
-		'host' => 'localhost',
 		'port' => 5432,
-		'database' => 'pg_sample',
-		'login' => 'postgres',
-		'password' => 'postgres',
-		'encoding' => 'utf8',
-	],
-	'SQLite' => [
-		'host' => 'localhost',
-		'database' => SQLITE_DB,
-		'login' => '',
-		'password' => '',
-		'encoding' => 'utf8',
-	],
-	'MySQL' => [
-		'host' => 'localhost',
-		'database' => 'my_sample',
-		'login' => 'admin',
-		'password' => 'admin',
-		'encoding' => 'utf8',
 	],
 ],
 'development' => [
 	'USE_DEBUGGER'	=> true,
 	'SESSION_LIMIT' => 'now +15 minute',	// for DEBUG
 	'Postgre' =>  [
-		// PostgreSQLの共通設定
-		'host' => 'localhost',
-		'database' => 'pg_sample',
-		'login' => 'postgres',
-		'password' => 'postgres',
-		'encoding' => 'utf8',
 		'Linux' => [		// OS別(PHP_OS)の設定
 			'port' => 5532,
 		],
@@ -55,20 +52,6 @@ const GlobalConfig = [
 			'login' => 'admin',		// 共通設定の上書き
 			'password' => 'admin',
 		],
-	],
-	'SQLite' => [
-		'host' => 'localhost',
-		'database' => SQLITE_DB,
-		'login' => '',
-		'password' => '',
-		'encoding' => 'utf8',
-	],
-	'MySQL' => [
-		'host' => 'localhost',
-		'database' => 'my_sample',
-		'login' => 'admin',
-		'password' => 'admin',
-		'encoding' => 'utf8',
 	],
 ],
 ];
