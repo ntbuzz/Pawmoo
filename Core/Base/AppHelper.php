989a0532cf854a9dc93171e00ffe8c8ba6b7e5ac
ntak <ntak@starship.net>
2022-01-31 23:43:13 +0900

    計測出力をデバッグ時に限定

--- Core/Base/AppHelper.php
+++ Core/Base/AppHelper.php
@@ -31,8 +31,10 @@ public function ViewTemplate($layout) {
 //==============================================================================
 // Runtime output
 public function Runtime() {
-	echo "<hr>\n";
-	sysLog::run_time(-1);
+	if(!is_bool_false(MySession::get_paramIDs('debugger'))) {
+		echo "<hr>\n";
+		sysLog::run_time(-1);
+	}
 }
 //==============================================================================
 // Resource(.css/.js) Output (Not USE!)
@@ -340,6 +342,15 @@ public static function define_var_array($arr,$name) {
 	$list = (empty($items)) ? '' : "'" . implode("',\n'",$arr) . "'";
 	$row = "var {$name} = [\n{$list}\n];";
 	return $row;
+}
+//==============================================================================
+// JS array define with KEY OBJECT
+public static function define_array_object($arr,$name) {
+	$txt = "var {$name} = [\n";
+	foreach($arr as $key => $val_arr) {
+		$txt = "{$txt}\"{$key}\": ['" . implode("',\n'",$val_arr)."'],\n";
+	}
+	return "{$txt}};";
 }
 //==============================================================================
 // ドロップダウンメニュー
