a271d70df1e6862580a88540a0c2bce5dd432233
ntak <ntak@starship.net>
2022-02-06 19:58:22 +0900

    不要関数削除

--- Core/Base/AppHelper.php
+++ Core/Base/AppHelper.php
@@ -30,12 +30,12 @@ public function ViewTemplate($layout) {
 }
 //==============================================================================
 // Runtime output
-public function Runtime() {
-	if(!is_bool_false(MySession::get_paramIDs('debugger'))) {
-		echo "<hr>\n";
-		sysLog::run_time(-1);
-	}
-}
+// public function Runtime() {
+// 	if(!is_bool_false(MySession::get_paramIDs('debugger'))) {
+// 		echo "<hr>\n";
+// 		sysLog::run_time(-1);
+// 	}
+// }
 //==============================================================================
 // Resource(.css/.js) Output (Not USE!)
 public function Resource($res) {
