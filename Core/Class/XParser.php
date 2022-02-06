82241175b951122d0663fd57e1d8f2e839ae8a19
ntak <ntak@starship.net>
2022-02-06 17:37:40 +0900

    新パーサー

---
+++ Core/Class/XParser.php
@@ -0,0 +1,177 @@
+<?php
+/* -------------------------------------------------------------
+ * PHPフレームワーク
+ *  SectionParser:  セクションレイアウトファイルの新単語パーサー
+ *		処理速度は遅くなるが、トークンを空白で区切る必要がなくなる
+ *      空白を含めるときはクオートで囲む
+ *      トークン:
+ *          "任意の文字列" または '任意の文字列'
+ *           <任意の文字列>
+ *          (任意の文字列)
+ *          //コメント文字列\r
+ */
+//==============================================================================
+class SectionParser {
+	private $lines;
+	private $line_count;
+	private $lno = 0;
+	private $current_line;
+	const token_separator = [
+		'=>' => 2,'/*' => 2,'*/' => 2,'//' => 2,
+		' ' => 1,"\t" => 1,"\n" => 1,
+	];
+	const skip_separator = [
+		' ' => 1,"\t" => 1,"\n" => 1,
+	];
+	const token_quote = [
+		'"' => '"',
+		"'" => "'",
+		'~' => '~',
+		'(' => ')',
+		'{' => '}',
+		'<' => '>',
+	];
+//==============================================================================
+    function __construct($template) {
+        $contents = file_get_contents($template);          // ファイルから全て読み込む (正規表現でトークン分離するため)
+        $this->lines = array_values(array_filter(explode("\n",$contents),'strlen'));
+		$this->line_count = count($this->lines);
+		$this->reset_line();
+	}
+//==============================================================================
+	private function reset_line() {
+		$this->lno = 0;
+		$this->next_line();
+	}
+//==============================================================================
+	private function next_line() {
+		if($this->lno >= $this->line_count) return false;
+		$this->current_line = $this->lines[$this->lno++];
+		return true;
+	}
+//==============================================================================
+	private function current_ch() {
+		if($this->current_line === true) {
+			if($this->next_line() === false) return false;
+		}
+		if($this->current_line === '') {
+			$this->current_line = true;
+			return "\n";
+		}
+		$ch = mb_substr($this->current_line,0,1);
+		$this->current_line = mb_substr($this->current_line,1);
+		return $ch;
+	}
+//==============================================================================
+	private function is_token($token) {
+		return isset(self::token_separator[$token]);
+	}
+//==============================================================================
+	private function is_separator() {
+		if($this->current_line === '') return true;//$this->next_line();
+		foreach(self::token_separator as $wd => $len) {
+			if(strncmp($this->current_line,$wd,$len) === 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+//==============================================================================
+	private function quote($q) {
+		$token = '';
+		$qend = self::token_quote[$q];
+		$ch = $this->current_ch();
+		$trim = ($ch === '^');
+		while($ch !== $qend) {
+			// エスケープ
+			if($ch === '\\') $ch = $this->current_ch();
+			$token = "{$token}{$ch}";
+			$ch = $this->current_ch();
+		}
+        // 戦闘の空白を削除
+        if($trim) $token = implode("\n", text_line_array("\n",mb_substr($token,1),FALSE));
+        // 改行文字\nを置換する
+        if($q === '"') $token = str_replace('\n', "\n", $token);
+        else if($q === '<') $token = "<{$token}>";	// タグクオート
+		return $token;
+	}
+//==============================================================================
+	public function get_token() {
+retry:	while(isset(self::skip_separator[$ch = $this->current_ch()])) ;
+		if($ch === false) return false;
+		$token = false;
+		do {
+			if(isset(self::token_quote[$ch])) {
+				$token = $this->quote($ch);
+			} else {
+				$token = $ch;
+				if($token === '[' || $token === ']') break;
+				$nest = 0;
+				while(!$this->is_token($token)) {
+					if($this->is_separator()) break;
+					// ] の先読み
+					if(mb_substr($this->current_line,0,1) === ']') {
+						if($nest-- === 0) break;	// name属性の外
+					}
+					if(($ch = $this->current_ch())===false) break;
+					if($ch === '[') ++$nest;
+					$token = "{$token}{$ch}";
+				}
+				if($token === '//') {
+					$this->next_line();
+					goto retry;
+				}
+				if($token === '/*') {
+					do {
+						$token = $this->get_token();
+					} while($token !== false && $token !== '*/');
+					goto retry;
+				}
+			}
+		} while($token === false);
+		return $token;
+	}
+//==============================================================================
+//  単語リストからセクション配列を作成し、配列要素を返す
+    function getSectionDef($is_TAG) {
+        $arr = [];
+		while(($wd=$this->get_token())!==false) {
+next_wd:    if($wd == ']') return $arr;            // セクション終了なら生成した配列を返す
+            if($wd == '[') {                       // セクション開始
+                do {
+                    $arr[] = $this->getSectionDef($is_TAG);    // セクション配列をとりだし、セクション配列に追加する
+                    $wd=$this->get_token();          // 次のトークン
+                } while( $wd == '[');                  // さらにセクション要素が続く間繰り返す
+                if($wd == ']') return $arr;            // セクション終了なら配列を返す
+            }
+            $nwd = $this->get_token();          // 次のトークン
+            if($nwd === '=>') {                          // 連想配列要素なら
+                $wkey = array_key_unique($wd,$arr);
+	            $wd = $this->get_token();          // 次のトークン
+                if($wd == '[') {                // セクション開始なら
+                    $arr[$wkey] = $this->getSectionDef($is_TAG);   // 再帰呼び出しでセクション要素を連想配列に代入
+                } else {
+                    $arr[$wkey] = $wd;                  // トークンを連想配列に代入
+                }
+            } else {
+                switch(is_tag_identifier($wd)) {
+                case 1: if($is_TAG===FALSE) {
+                            $arr[] = $wd;
+                            break;
+                        }
+                case 2:
+                case 3:
+                        $arr[$wd] = '';
+                        break;
+                case 0: 
+					if(mb_substr($wd,0,1) === '\\') $wd = mb_substr($wd,1);
+                    $arr[] = $wd; break;
+                }
+				if($nwd === false) break;
+				$wd = $nwd;
+				goto next_wd;
+            }
+        }
+       return $arr;
+    }
+}
