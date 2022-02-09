c2e49febc55fb1323dec733b96cfc2c9a544d492
ntak <ntak@starship.net>
2022-02-09 22:49:24 +0900

    POST変数が配列の時のバグ修正

--- Core/Common/coreLibs.php
+++ Core/Common/coreLibs.php
@@ -53,8 +53,10 @@ function xchange_Boolean($arr) {
 	$data = [];
 	$bool_value = [ 'on' => TRUE,'off' => FALSE,'t' => TRUE,'f' => FALSE];
 	foreach($arr as $key => $val) {		// GET parameter will be check query
-		if(array_key_exists($val,$bool_value)) $val = $bool_value[$val];
-		else if(is_numeric($val)) $val = intval($val);
+		if(is_scalar($val)) {
+			if(array_key_exists($val,$bool_value)) $val = $bool_value[$val];
+			else if(is_numeric($val)) $val = intval($val);
+		}
 		if(ctype_alnum(str_replace(['-','_'],'', $key))) $data[$key] = $val;
 	}
 	return $data;
