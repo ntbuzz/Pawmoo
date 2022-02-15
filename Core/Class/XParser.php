1a5cdce5ee53db88b5a3678921408dc19ce8953e
ntak <ntak@starship.net>
2022-02-15 12:48:31 +0900

    コメントトークンの処理統合

--- Core/Class/XParser.php
+++ Core/Class/XParser.php
@@ -17,8 +17,8 @@ class SectionParser {
 	private $lno = 0;
 	private $current_line;
 	const token_separator = [
-		'=>' => 2,'/*' => 2,'*/' => 2,	// '//' => 2,	http:// の回避のため
-		' ' => 1,"\t" => 1,"\n" => 1,
+//		/* と // は特別処理
+		'=>' => 2,'*/' => 2,' ' => 1,"\t" => 1,"\n" => 1,
 	];
 	const skip_separator = [
 		' ' => 1,"\t" => 1,"\n" => 1,
