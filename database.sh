cb46bc7d8f4bcbafad0a238158aa6cabeb12b6d5
ntak <ntak@starship.net>
2022-02-26 13:33:48 +0900

    バッチ化

--- database.sh
+++ database.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+!/usr/bin/sh
 
 php utl/database.php $1/$2
 
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+# setup cmd app
+#		cmd:
+#			create		フォルダツリーを作成
+#			schema		スキーマファイルを作成
+#			gen			Model/Lang ファイルを作成
+#			table		テーブルとビューを作成
+#			view		ビューのみ作成
+
+php Core/Tools/Setup.php $@
+
