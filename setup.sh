14c7cc698871291a7f9a9fd48be0d28368ff6b77
ntak <ntak@starship.net>
2022-03-04 02:36:19 +0900

    コマンドシンタックス記述

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/sh
-# setup cmd app
-#		cmd:
-#			create		フォルダツリーを作成
-#			schema		スキーマファイルを作成
-#			gen			Model/Lang ファイルを作成
-#			table		テーブルとビューを作成
-#			view		ビューのみ作成
+# setup cmd app [module]
+#  cmd:
+#	create app module	アプリケーションフォルダと仕様フォルダを作成する
+#						moduleまで指定するとmoduleコマンドも実行する
+#	module app module	モジュールフォルダを作成する
+#	spec app 			仕様フォルダ(appSpec)にアプリ用フォルダを作成する
+#	schema app module	テーブル仕様(仕様CSV)からモデルスキーマと言語リソースを生成する
+#						moduleを省略すると仕様フォルダ全ての仕様CSVを変換する
+#	model app module	モデルスキーマからモデルクラスを生成する
+#	setup app module	schemaコマンドとmodelコマンドを連続実行する
+#	table app module	モデルスキーマからテーブルとビューを生成する
+#						InitCSVのあるモデルはCSVの読込みも実行する
+#	view app module		モデルスキーマからビューを生成する
+#	csv app module		モデルスキーマにInitCSVがあればCSV読込みを実行する
+#
 
 php Tools/cmd/Setup.php $@
 
