3476d1dcd5c5ca74e177c336ee32f34817b5838e
ntak <ntak@starship.net>
2022-03-14 21:42:16 +0900

    閉じカッコのミス

--- Tools/Template/AppModel.php
+++ Tools/Template/AppModel.php
@@ -5,7 +5,6 @@ class %model%Model extends AppModel2 {
 static $DatabaseSchema = [
 %databasedefs%
 	];
-}
 //==============================================================================
 //	クラス初期化処理
 protected function ClassInit() {
