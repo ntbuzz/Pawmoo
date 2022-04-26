dbebc626bd071b863cf377cc124a9d33b2125dac
ntak <ntak@starship.net>
2022-04-26 20:52:17 +0900

    共有セッションによるログイン継承

--- Core/Base/AppController.php
+++ Core/Base/AppController.php
@@ -14,6 +14,7 @@ class AppController extends AppObject {
 	protected $LoggingMethod = NULL;	// execute log save Model "class.method". ex. 'Access.Logging'
 	protected $BypassMethod = '';		// Login bypass method,if NEDD LOGIN(ex.Logout)
 	protected $AutoLogin = '';		    // DefaultUser AutoLogin method,if NEDD LOGIN(ex. MailsendAction)
+	protected $import_login = false;	// Import from share-memory Login Info from Other Apps
 	private $orgModel;					// save original Model Class for Spoofing
 //==============================================================================
 // constructor: create MODEL, VIEW(with HELPER)
@@ -73,37 +74,41 @@ public function is_enable_action($action) {
 //==============================================================================
 // authorised login mode, if need view LOGIN form, return FALSE
 public function is_authorised($method) {
+	$sh_login = MySession::getShmData('Login',true);	// check Login-Info from other App,and remove 'Login'
+	if($this->import_login) {
+		if(!empty($sh_login)) MySession::set_LoginValue($sh_login);
+	}
 	if(defined('LOGIN_CLASS')) {			// enable Login Class
 		$model = LOGIN_CLASS . 'Model';
 		$Login = $this->$model;
 		$login_key = isset($Login->LoginID) ? $Login->LoginID:'login-user';
-			$autologin = (is_array($this->AutoLogin)) ? $this->AutoLogin : [$this->AutoLogin];
+		$autologin = (is_array($this->AutoLogin)) ? $this->AutoLogin : [$this->AutoLogin];
 		$bypass_method = (is_array($this->BypassMethod)) ? $this->BypassMethod : [$this->BypassMethod];
 		$bypass_method[] = 'Logout';	// must be append LogoutAction
-			$pass_check = false;	// NO password check.
-			if(in_array($method,$autologin) || CLI_DEBUG) {
-				$data = $Login->defaultUser();		// auto-login user
-				if(isset(App::$Post['login'])) $data = array_override($data,App::$Post);
-			} else if(isset(App::$Post['login'])) {	// Distinction Normal Login POST
-				$data = App::$Post;
-				$pass_check = true;
-			} else {	// check already LOGIN
-				$login = MySession::get_LoginValue([$login_key,'LANG','REGION']);
-				$data = array_combine([$login_key,'language','region'],$login);
-			}
+		$pass_check = false;	// NO password check.
+		if(in_array($method,$autologin) || CLI_DEBUG) {
+			$data = $Login->defaultUser();		// auto-login user
+			if(isset(App::$Post['login'])) $data = array_override($data,App::$Post);
+		} else if(isset(App::$Post['login'])) {	// Distinction Normal Login POST
+			$data = App::$Post;
+			$pass_check = true;
+		} else {	// check already LOGIN
+			$login = MySession::get_LoginValue([$login_key,'LANG','REGION']);
+			$data = array_combine([$login_key,'language','region'],$login);
+		}
 		$udata = $Login->is_validLoginUser($data,$pass_check);	// is valid user check
-			if($udata === false) {	// fail valid user => not login, or unknown user
+		if($udata === false) {	// fail valid user => not login, or unknown user
 			if($this->needLogin && !in_array($method,$bypass_method)) {
-					$userid = (isset($data[$login_key])) ? $data[$login_key] : '';
-					$login_page = (defined('LOGIN_PAGE')) ? LOGIN_PAGE : 'app-login.php';
-					page_response($login_page,$Login->retryMessages($userid));
-					// NEVER RETURN HERE!
-					return FALSE;
-				}
+				$userid = (isset($data[$login_key])) ? $data[$login_key] : '';
+				$login_page = (defined('LOGIN_PAGE')) ? LOGIN_PAGE : 'app-login.php';
+				page_response($login_page,$Login->retryMessages($userid));
+				// NEVER RETURN HERE!
+				return FALSE;
+			}
 			// not login, and this method is BYPASS
 			$udata = MySession::get_LoginValue([$login_key,'LANG','REGION']);
 			$udata[0] = NULL;	// no login user
-			}
+		}
 		// setup login user language and region
 		list($userid,$lang,$region) = $udata;
 		$login_data = [
@@ -362,6 +367,16 @@ public function LanguageAction() {
 	];
 	MySession::set_LoginValue($login_data);
 	$this->set_location($refer);
+}
+//==============================================================================
+// Application switch with  Login Session
+public function RelocateAction() {
+	if(isset(MySession::$EnvData['Login'])) {
+		MySession::$ShmData['Login'] = MySession::$EnvData['Login'];
+	}
+	list($app,$cont,$act) = array_alternative(App::$Filters,3,[$this->ModuleName,'index','list']);
+	$url = "/{$app}/{$cont}/{$act}/";
+	header('Location:' . $url);
 }
 //==============================================================================
 //	List Condition, must be override Controller
