128ade8b6e35ffd92ba767b10e92698f5889453a
ntak <ntak@starship.net>
2022-05-11 22:58:38 +0900

    エスケープ変換の統合

--- Core/Handler/SQLiteHandler.php
+++ Core/Handler/SQLiteHandler.php
@@ -75,8 +75,13 @@ public function getLastError() {
 //==============================================================================
 //	レコードの追加 
 //==============================================================================
+private function safe_convert($row,$func) {
+	$row = $this->sql_str_quote($row,"'","''");	// 書き込み型変換
+	return $this->sql_safe_convert($row);	// 書き込み型変換
+}
+//==============================================================================
 public function insertRecord($row) {
-	$row = $this->sql_safe_convert($this->sql_str_quote($row,["'"],["''"]));	// 書き込み型変換
+	$row = $this->safe_convert($row);
 	// UPDATE OR INSERT => REPLACE SQL生成
 	$kstr = '"' . implode('","', array_keys($row)) . '"';
 	$vstr = implode(",", $row);
@@ -96,7 +101,7 @@ public function insertRecord($row) {
 //	レコードの更新 $row[key] value
 //==============================================================================
 public function updateRecord($wh,$row) {
-	$row = $this->sql_safe_convert($this->sql_str_quote($row,["'"],["''"]));	// 書き込み型変換
+	$row = $this->safe_convert($row);
 	list($pkey,$pval) = array_first_item($wh);
 	unset($row[$pkey]);			// プライマリキーは削除しておく
 	$where = " WHERE \"{$pkey}\"={$pval}";		// プライマリキー名を取得
@@ -122,7 +127,7 @@ public function updateRecord($wh,$row) {
 //	on confilict は SQLite 3.24.0 以降
 public function upsertRecord($wh,$row) {
 	$row = array_merge($wh,$row);			// INSERT 用にプライマリキー配列とデータ配列をマージ
-	$row = $this->sql_safe_convert($this->sql_str_quote($row,["'"],["''"]));	// 書き込み型変換
+	$row = $this->safe_convert($row);
 	list($pkey,$pval) = array_first_item($wh);
 	// UPDATE OR INSERT => REPLACE SQL生成
 	$kstr = '"' . implode('","', array_keys($row)) . '"';
