91e18cfcf0765ae8c86a759283f7a131f13f5928
ntak <ntak@starship.net>
2022-06-05 07:52:32 +0900

    単独属性指定のバグ修正

--- Core/Base/AppView.php
+++ Core/Base/AppView.php
@@ -376,8 +376,8 @@ public function ViewTemplate($name,$vars = []) {
         if(!empty($attrs)) {
             ksort($attrs);
             foreach($attrs as $name => $val) {
-				if($val === [] || $val === NULL) $attr .= " {$name}"; 
-				else {
+				if($val === NULL) $attr .= " {$name}"; 
+				else if(!empty($val)) {
 					$str = (is_array($val)) ? implode("",$val) :$val;
 					$str = $this->expand_Strings($str,$vars);
 					$quote = mb_substr($str,0,1).mb_substr($str,-1);
