773d880e2f3521ebdb1ff5cbe0f79ff5fe1758d7
ntak <ntak@starship.net>
2022-06-15 21:14:25 +0900

    チェック文字種を追加

--- Core/Template/res/js/window.js
+++ Core/Template/res/js/window.js
@@ -33,8 +33,8 @@ $(document).on('change','.combobox>select',function () {
 // INPUT 文字種チェック
 $(document).on('blur', 'input[type="text"]', function (e) {
 	var TypeObj = {
-		'numeric':	['#', '${#core.NUMERIC}'],		// INPUT 数字
-		'number':	['0', '${#core.NUMERIC}'],		// INPUT 整数
+		'numeric':	['#', '${#core.NUMERIC}'],		// INPUT 数値
+		'number':	['0', '${#core.NUMBER}'],		// INPUT 整数
 		'date':		['@', '${#core.DATEFMT}'],		// INPUT 日付
 		'datetime':	[':', '${#core.TIMESTAMP}'],	// INPUT 日時
 		'alpha':	['Aa','${#core.ALPHABET}'],		// INPUT 英字
@@ -55,29 +55,5 @@ $(document).on('blur', 'input[type="text"]', function (e) {
 	};
 	return false;
 });
-// // 文字種チェック
-// function InputCharsetCheck(target, kind, msg) {
-// 	var str = target.val().hankaku();
-// 	switch (str.charsetCheck(kind)) {
-// 		case true: target.val(str); break;
-// 		case false:
-// 			$.dialogBox('${#core.CAUTION}', msg, false, function () { target.focus(); target.select();});
-// 			break;
-// 	};
-// 	return false;
-// };
-// // INPUT 数字
-// $(document).on('blur', 'input.numeric[type="text"]', function (e) {
-// 	InputCharsetCheck($(this), '#', '${#core.NUMERIC}');
-// 	return false;
-// });
-// // INPUT 日付
-// $(document).on('blur', 'input.date[type="text"]', function () {
-// 	InputCharsetCheck($(this), '@', '${#core.DATEFMT}');
-// });
-// // INPUT 日時
-// $(document).on('blur', 'input.datetime[type="text"]', function () {
-// 	InputCharsetCheck($(this), ':', '${#core.TIMESTAMP}');
-// });
 // ウィンドウサイズ調整
 $(window).resize();
